729e46a77a2eddca3ae330312834866408e26e15
Shai Daniel <155896805+shaid2233@users.noreply.github.com>
2025-06-25 14:38:37 +0300

    Delete opentelemetry_offline_installer directory

--- opentelemetry_offline_installer/install_k9s.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-echo "🔧 Installing K9s..."
-tar -xvzf bin/k9s_Linux_amd64.tar.gz -C bin/
-sudo mv bin/k9s /usr/local/bin/
-sudo chmod +x /usr/local/bin/k9s
-echo "✅ K9s installed!"
